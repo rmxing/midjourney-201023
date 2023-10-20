6ef8eb682eb22d7fc57b87d3cba244954200d771
Teoh TeikToe <ttt@mensa.org.sg>
2023-10-20 12:01:49 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+from flask import render_template,request
+import json
+import time
+import requests
+
+app = Flask(__name__)
+
+headers = {
+    "Authorization" : "Token 787f515cb0624813736c11e7fefec66473394f02",
+    "Content-Type" : "application/json"
+}
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        q = request.form.get("question")
+        print(q)
+        body = json.dumps(
+            {
+                "version" : "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+                "input"   : 
+                {
+                    "prompt" : q
+                }
+            }
+        )
+        output = requests.post("https://api.replicate.com/v1/predictions",data=body,headers=headers)
+        time.sleep(10)
+        get_url = output.json()["urls"]["get"]
+        get_result = requests.post(get_url,headers=headers).json()['output']
+        return(render_template("index.html",result=get_result[0]))
+    else:
+        return(render_template("index.html",result="waiting........"))
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
